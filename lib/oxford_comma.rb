34cddb64a0533ebbe200c5a1668c2af9f19666d5
Ryan Gill <rdgill95@gmail.com>
2018-12-28 16:19:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,6 @@ def oxford_comma(array)
  elsif array.size == 2 
     array.join(" and ")
  else array.size == 3
-   array.join[1](", ")
-   array.join[2](", ")
-   array.join[3](" and ")
+   array.join[1](" , ")
  end
 end
